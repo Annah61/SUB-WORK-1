0b2eea3621da7eab74858a6952856a55493a8dd8
Annah61 <annahmoore.morerwa@gmail.com>
2023-09-09 23:35:44 +0200

    Add project files.

---
+++ SUB WORK 1/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnpress = New Button()
+        SuspendLayout()
+        ' 
+        ' btnpress
+        ' 
+        btnpress.Location = New Point(258, 177)
+        btnpress.Name = "btnpress"
+        btnpress.Size = New Size(177, 34)
+        btnpress.TabIndex = 0
+        btnpress.Text = "Press Me"
+        btnpress.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnpress)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnpress As Button
+End Class
---
+++ SUB WORK 1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub btnpress_Click(sender As Object, e As EventArgs) Handles btnpress.Click
+        hello()
+    End Sub
+
+    Sub hello()
+        MsgBox("Hello Annah,I hope you are well")
+    End Sub
+
+
+End Class
